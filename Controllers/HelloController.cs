81649b3a3cbb77706f3700c9ffc4c26143cd88ff
sas03 <stephanesumo@yahoo.fr>
2023-07-04 14:52:14 +0200

    ASP.NET MVC Routage

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Routage.Controllers
+{
+    public class HelloController : Controller
+    {
+        public string SayHello()
+        {
+            return "Hello !";
+        }
+        public string YellHello()
+        {
+            return "HELLO !";
+        }
+        [Route("/Say")]
+        public string Say()
+        {
+            return "Hello !";
+        }
+        [Route("/Yell")]
+        public string Yell()
+        {
+            return "HELLO !";
+        }
+    }
+}
